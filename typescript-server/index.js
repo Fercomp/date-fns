f077af8aabf95e4d8b35e609ff8cc2ad449d83d6
Bassani-Paulo <p185448@dac.unicamp.br>
2024-07-06 20:59:45 -0300

    a

--- typescript-server/index.js
+++ typescript-server/index.js
@@ -5,14 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const isWeekend_1 = require("./../date-fns/src/isWeekend");
-const data = new Date("2024-03-25");
-//const data = new Date();
+const getDay_1 = require("./../date-fns/src/getDay");
 const port = 8000;
 const app = (0, express_1.default)();
-const days = [1, 2, 5, 6];
-const todayIsWeekend = (0, isWeekend_1.isWeekend)(data, { weekendDays: days });
+const format = [" Sunday", " Monday", " Tuesday", " Wednesday", " Thursday", " Friday", " Saturday"];
+const date = new Date();
+//const date = new Date("2024-01-02");
+const days = [0, 1];
+const todayIsWeekend = (0, isWeekend_1.isWeekend)(date, { weekendDays: days });
+//const todayIsWeekend = isWeekend(date)
 app.get("/", (req, res) => {
-    res.send(`${data} is weekend ${todayIsWeekend}`);
+    res.send(`Is ${format[(0, getDay_1.getDay)(date)]} weekend? <br>
+    ${todayIsWeekend}`);
 });
 app.listen(port, () => {
     console.log(" Server rodando");
--- typescript-server/index.ts
+++ typescript-server/index.ts
@@ -1,19 +1,24 @@
 import express, {Express, Request, Response } from "express";
 import { isWeekend } from "./../date-fns/src/isWeekend";
-import type { WeekOptions, Day } from "./../date-fns/src/types.js";
+import { getDay } from "./../date-fns/src/getDay";
+import type { Day } from "./../date-fns/src/types.js";
 
 
 
-const data = new Date("2024-03-25");
-//const data = new Date();
+
 const port = 8000;
-
 const app: Express = express();
-const days:Day[] = [1,2,5,6]
-const todayIsWeekend = isWeekend(data, {weekendDays:days})
+const format = [" Sunday", " Monday", " Tuesday", " Wednesday", " Thursday", " Friday", " Saturday"]
+
+const date = new Date();
+//const date = new Date("2024-01-02");
+const days:Day[] = [0, 1]
+const todayIsWeekend = isWeekend(date, {weekendDays:days})
+//const todayIsWeekend = isWeekend(date)
 
 app.get("/", (req: Request, res: Response) => {
-    res.send(`${data} is weekend ${todayIsWeekend}`);
+    res.send(`Is ${format[getDay(date)]} weekend? <br>
+    ${todayIsWeekend}`);
 });
 
 app.listen(port, () => {
