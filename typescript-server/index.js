35c988d3ea15b831c73ef322d3d2f759aaa5a403
Bassani-Paulo <p185448@dac.unicamp.br>
2024-07-07 16:26:24 -0300

    a

--- typescript-server/index.js
+++ typescript-server/index.js
@@ -4,17 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const lastDayOfMonth_1 = require("./../date-fns/src/lastDayOfMonth");
 const isLeapYear_1 = require("./../date-fns/src/isLeapYear");
-const data = new Date('300-07-01');
 const port = 8000;
 const app = (0, express_1.default)();
-const day = (0, lastDayOfMonth_1.lastDayOfMonth)(data).getDate();
-const considerGregorian = true;
+const data = new Date('300-02-02');
 app.get("/", (req, res) => {
     const result = (0, isLeapYear_1.isLeapYear)(data, true);
-    res.send(`Is leap year: ${result}`);
+    res.send(`Is leap year? <br>
+    ${result}`);
 });
 app.listen(port, () => {
-    console.log("Server rodando ...3");
+    console.log("Server rodando");
 });
--- typescript-server/index.ts
+++ typescript-server/index.ts
@@ -1,18 +1,17 @@
 import express, {Express, Request, Response } from "express";
-import { lastDayOfMonth } from "./../date-fns/src/lastDayOfMonth";
 import { isLeapYear } from "./../date-fns/src/isLeapYear";
-const data = new Date('300-07-01')
-const port = 8000;
 
+const port = 8000;
 const app: Express = express();
-const day = lastDayOfMonth(data).getDate()
-const considerGregorian = true 
+
+const data = new Date('300-02-02')
 
 app.get("/", (req: Request, res: Response) => {
     const result = isLeapYear(data, true);
-    res.send(`Is leap year: ${result}`);
+    res.send(`Is leap year? <br>
+    ${result}`);
 });
 
 app.listen(port, () => {
-    console.log("Server rodando ...3");
+    console.log("Server rodando");
 });
